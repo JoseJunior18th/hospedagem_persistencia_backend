251a09c269b21e461c0c4ca0e2dd90f9b77a233f
José Costa <80170575+JoseJunior18th@users.noreply.github.com>
2025-02-21 18:17:38 -0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS activities (
     id SERIAL PRIMARY KEY,
     categoria VARCHAR(255),
     classificacao VARCHAR(255),
     descricao VARCHAR(255),
     horas VARCHAR(10),
     horas_aproveitadas VARCHAR(10)
-)
+);
